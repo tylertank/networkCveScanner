7d9143acdc069ee1534475ddcd1f010372be10d5
Stephanie Smith <u1179341@utah.edu>
2023-04-09 12:44:17 -0600

    Added networkStreamExtensions.cs from Trace's window daemon.  Worked on threading the networking method and seperating code into functions for clarity and usage.

--- ReCVEServer/Networking/Server.cs
+++ ReCVEServer/Networking/Server.cs
@@ -1,8 +1,11 @@
 ﻿using ReCVEServer.Data;
 using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
+using ReCVEServer.Networking.Extensions;
 
 namespace ReCVEServer.Networking
 {
@@ -14,6 +17,9 @@ namespace ReCVEServer.Networking
         public Server( IServiceScopeFactory serviceScopeFactory)//ReCVEServerContext context,
         {
             _scopeFactory = serviceScopeFactory;
+
+           
+
             serverSock();
            // _context = context;
         }
@@ -27,24 +33,25 @@ namespace ReCVEServer.Networking
             using (var scope = _scopeFactory.CreateScope())
             {
                 var context = scope.ServiceProvider.GetService<ReCVEServerContext>();
-                var contextList = context.Softwares.ToList();
+               // var contextList = context.Softwares.ToList();
 
                 System.Diagnostics.Debug.WriteLine("I made it into serverSock");
-                IPHostEntry entry = await Dns.GetHostEntryAsync("localhost");
-                IPAddress iPAddress = entry.AddressList[0];
-                System.Diagnostics.Debug.WriteLine(entry.AddressList[0]);
                 IPEndPoint epEndPoint = new(IPAddress.Any, 5004);
-
                 TcpListener listener = new(epEndPoint);
+
                 System.Diagnostics.Debug.WriteLine("We're now waiting for a connection");
                 listener.Start(10);
+
                 while (true)
                 {
                     using TcpClient handler = await listener.AcceptTcpClientAsync();
-                    System.Diagnostics.Debug.WriteLine("We got a connection");
-                    await using NetworkStream stream = handler.GetStream();
 
-                    string message = "hello world";
+                    Thread t = new Thread(new ParameterizedThreadStart(directClient));
+                    t.Start(handler);
+
+                   
+
+                    /*string message = "hello world";
                     var enMessage = Encoding.UTF8.GetBytes(message);
                     byte[] buffer2 = new byte[1024];
                     await stream.ReadAsync(buffer2);
@@ -54,14 +61,38 @@ namespace ReCVEServer.Networking
                     System.Diagnostics.Debug.WriteLine("Sending first message to client " + message + "\n");
                     await stream.WriteAsync(enMessage);
 
-                    byte[] buffer = new byte[102];
+                    byte[] buffer = new byte[1024];
                     await stream.ReadAsync(buffer);
                     Decoder d = Encoding.UTF8.GetDecoder();
                     System.Diagnostics.Debug.WriteLine("recieving second message from client " + Encoding.UTF8.GetString(buffer) + "\n\n");
-                    handler.Close();
+                    handler.Close();*/
                 }
 
             }
+        }
+        public async void directClient(object obj)
+        {
+            var enMessage = Encoding.UTF8.GetBytes("hello world");
+            byte[] buffer = new byte[1024];
+
+            TcpClient handler = (TcpClient)obj;
+           NetworkStream stream = handler.GetStream();
+
+            await stream.ReadExactlyAsync(buffer, 0, 4);
+            int dataLength = BitConverter.ToInt32(buffer, 0);
+
+
+            await stream.ReadAsync(buffer);
+            //System.Diagnostics.Debug.WriteLine("Recieving first message from client " + Encoding.UTF8.GetString(buffer2) + "\n\n");
+
+
+
+            await stream.WriteAsync(enMessage);
+            await stream.ReadAsync(buffer);
+            Decoder d = Encoding.UTF8.GetDecoder();
+            System.Diagnostics.Debug.WriteLine("recieving second message from client " + Encoding.UTF8.GetString(buffer) + "\n\n");
+            handler.Close();
+           
         }
     }
 }
