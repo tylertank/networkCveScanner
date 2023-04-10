770c42298b2e4b96b4dac6ea0a81fa06bcdedbd5
Stephanie Smith <u1179341@utah.edu>
2023-04-10 13:36:09 -0600

    I added a couple of new functions to allow the jsons recieved from the clients to be processed. This does not sustain a long term connection which is something I'll have to work on.

--- ReCVEServer/Networking/Server.cs
+++ ReCVEServer/Networking/Server.cs
@@ -6,6 +6,8 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using ReCVEServer.Networking.Extensions;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
 
 namespace ReCVEServer.Networking
 {
@@ -14,26 +16,23 @@ namespace ReCVEServer.Networking
     {
         private readonly ReCVEServerContext _context;
         private readonly IServiceScopeFactory _scopeFactory;
-        public Server( IServiceScopeFactory serviceScopeFactory)//ReCVEServerContext context,
+        public Server(IServiceScopeFactory serviceScopeFactory)//ReCVEServerContext context,
         {
             _scopeFactory = serviceScopeFactory;
-
-           
-
             serverSock();
-           // _context = context;
+            // _context = context;
         }
         public async Task StartAsync()
         {
 
             Console.WriteLine("\n\n\nI made it to start async\n\n\n");
         }
-       public async void serverSock()
+        public async void serverSock()
         {
             using (var scope = _scopeFactory.CreateScope())
             {
                 var context = scope.ServiceProvider.GetService<ReCVEServerContext>();
-               // var contextList = context.Softwares.ToList();
+                // var contextList = context.Softwares.ToList();
 
                 System.Diagnostics.Debug.WriteLine("I made it into serverSock");
                 IPEndPoint epEndPoint = new(IPAddress.Any, 5004);
@@ -44,55 +43,119 @@ namespace ReCVEServer.Networking
 
                 while (true)
                 {
-                    using TcpClient handler = await listener.AcceptTcpClientAsync();
+                    TcpClient handler = await listener.AcceptTcpClientAsync();
 
                     Thread t = new Thread(new ParameterizedThreadStart(directClient));
                     t.Start(handler);
-
-                   
-
-                    /*string message = "hello world";
-                    var enMessage = Encoding.UTF8.GetBytes(message);
-                    byte[] buffer2 = new byte[1024];
-                    await stream.ReadAsync(buffer2);
-                    System.Diagnostics.Debug.WriteLine("Recieving first message from client " + Encoding.UTF8.GetString(buffer2) + "\n\n");
-
-
-                    System.Diagnostics.Debug.WriteLine("Sending first message to client " + message + "\n");
-                    await stream.WriteAsync(enMessage);
-
-                    byte[] buffer = new byte[1024];
-                    await stream.ReadAsync(buffer);
-                    Decoder d = Encoding.UTF8.GetDecoder();
-                    System.Diagnostics.Debug.WriteLine("recieving second message from client " + Encoding.UTF8.GetString(buffer) + "\n\n");
-                    handler.Close();*/
                 }
 
             }
         }
+        //This method sends the json to the appropiate method to be parsed and later stored in the database
         public async void directClient(object obj)
         {
-            var enMessage = Encoding.UTF8.GetBytes("hello world");
-            byte[] buffer = new byte[1024];
-
             TcpClient handler = (TcpClient)obj;
-           NetworkStream stream = handler.GetStream();
+            NetworkStream stream = handler.GetStream();
+            Task<string> jString = ReceiveData(stream);
+            string jsonS = jString.Result;
+            var jResults = JObject.Parse(jsonS);
+            var IDVal =jResults.GetValue("id");
+            var typeVal = jResults.GetValue("type");
+            //System.Diagnostics.Debug.WriteLine(print);
+            if (jResults.Value<string>("id") == "null")
+            {
+                clientHandshake(jResults);
+            }
+            else if (jResults.Value<string>("type") == "scan")
+            {
+                processScan(jResults);
+            }
+            else if(jResults.Value<string>("type") == "process")
+            {
+                processStatus(jResults);
+            }
+           handler.Close();
+        }
+        public async void clientHandshake(JObject jResults)
+        {
+            System.Diagnostics.Debug.WriteLine("made it to client handshake");
+   
+            var info = jResults.GetValue("info");
+            var temp = info.First();
+            var computer = temp.Value<string>("computer");
+            var ip = temp.Value<string>("ip");
+            
+            //process client hello and get computer addresss and ip out of it
+            //create serverAck with client id
+            //send server ack to cilent
+        }
+        public async void processScan(JObject jResults)
+        {
+            System.Diagnostics.Debug.WriteLine("made it to process scan");
+            int id = jResults.Value<int>("id");
+            var objects = jResults.GetValue("objects");
+            for( int i=0; i < objects.Count(); i++)
+            {
+                //do something with each object
+                System.Diagnostics.Debug.WriteLine(i);
+            }
+            //how do we store vendor application version?
+            //maybe just an array?
+            
+            //process scan and put in database
+        }
+        public async void processStatus(JObject jResults)
+        {
+            System.Diagnostics.Debug.WriteLine("made it to process status");
+            int id = jResults.Value<int>("id");
+            var objects = jResults.GetValue("objects");
+            for (int i = 0; i < objects.Count(); i++)
+            {
+                //do something with each object
+                System.Diagnostics.Debug.WriteLine(i);
+            }
+            //process status and put in data base
+        }
 
+
+
+
+
+        //***************************************************************************************************************************
+        //These methods are modified fromm the windows daemon created by trace engel
+        
+        private void SendData(string message, NetworkStream stream)
+        {
+            byte[] data = new byte[message.Length + 4];
+
+            byte[] count_bytes = BitConverter.GetBytes(message.Length);
+            for (int i = 0; i < 4; i++)
+            {
+                data[i] = count_bytes[i];
+            }
+            Encoding.UTF8.GetBytes(message, 0, message.Length, data, 4);
+
+            //NetworkStream stream = _tcpSocket.GetStream();
+            //try
+            {
+                stream.Write(data, 0, data.Length);
+            }
+        }
+        private async Task<string> ReceiveData(NetworkStream stream)
+        {
+            byte[] buffer = new byte[1024]; // Note the buffer is never cleared. It is simply overwritten with new data
+                                            // and only the new data is read.
             await stream.ReadExactlyAsync(buffer, 0, 4);
             int dataLength = BitConverter.ToInt32(buffer, 0);
-
-
-            await stream.ReadAsync(buffer);
-            //System.Diagnostics.Debug.WriteLine("Recieving first message from client " + Encoding.UTF8.GetString(buffer2) + "\n\n");
-
-
-
-            await stream.WriteAsync(enMessage);
-            await stream.ReadAsync(buffer);
-            Decoder d = Encoding.UTF8.GetDecoder();
-            System.Diagnostics.Debug.WriteLine("recieving second message from client " + Encoding.UTF8.GetString(buffer) + "\n\n");
-            handler.Close();
-           
+            StringBuilder sb = new();
+            while (dataLength > 0)
+            {
+                int bytesRead = await stream.ReadAsync(buffer, 0, dataLength < 1024 ? dataLength : 1024);
+                sb.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+                dataLength -= bytesRead;
+            }
+            return sb.ToString();
         }
     }
 }
+
