a7a826966f3d3bcf67a5f5526a1833bde83ad037
Stephanie Smith <u1179341@utah.edu>
2023-04-20 15:25:24 -0600

    Filled out serverCommand class. Commented out dummy data in ReCVEServerContext and Program.cs. In server.direct client I reworked the client hello so it wasn't so bulky in the function. I did this by adding the method startHandshake. I also added a try catch loop in direct client so clients can safely disconnect.

--- ReCVEServer/Networking/ServerCommand.cs
+++ ReCVEServer/Networking/ServerCommand.cs
@@ -1,4 +1,11 @@
 ﻿namespace ReCVEServer.Networking {
     public class ServerCommand {
+        public string type { get; private set; }
+       
+        public string command { get; set; }
+        public int frequency { get; set; }
+        public ServerCommand() {
+            type = "serverCommand";
+        }
     }
 }
