90b9678e88989389b5337a743ed209f48c0cb52e
Stephanie Smith <u1179341@utah.edu>
2023-04-23 16:05:49 -0600

    Pushed a data base migration so that a client will not add a specific software twice. Also added a scan request after the client hello for the client.

---
+++ ReCVEServer/Migrations/20230423005407_updateSoftware.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReCVEServer.Migrations
+{
+    public partial class updateSoftware : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint("UK_Software", "Software", new string[4]{ "client", "vendor", "application", "version"});
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
